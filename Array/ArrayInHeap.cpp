70adb4f779977df240e5fe4cb327364f67e0e37f
Mahesh Kamble <90838761+kamblemaheshg@users.noreply.github.com>
2022-04-02 10:37:51 +0530

    Create ArrayInHeap.cpp

---
+++ Array/ArrayInHeap.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ 
+    int *A;
+    int n;
+    cin >> n;
+    A = new int[n];
+    
+    for (int i = 0; i < n ; i++) {
+        cin >> A[i] ;
+    }
+    
+    for (int i = 0; i < n ; i++) {
+        cout << A[i] << "\n";
+    }
+
+    return 0;
+}
