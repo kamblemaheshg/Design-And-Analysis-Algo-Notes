617623241202c07d692fe28e281933edff16bf61
Mahesh Kamble <90838761+kamblemaheshg@users.noreply.github.com>
2022-04-02 10:32:45 +0530

    Create ArrayInStack.cpp

---
+++ Array/ArrayInStack.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ 
+    
+    int A[6]={10,50,40,30,20,50};
+    for (int i = 0; i < 6 ; i++) {
+        cout << A[i] <<endl;
+    }
+
+    return 0;
+}
